30a616071ad6e37665c570c30e2e68bf3c31354e
Rúben Gomes <gomesruben21@gmail.com>
2019-01-05 01:31:28 +0000

    Add new middleware dependencies to setup our app

--- node-with-react-fullstack-web-dev/emaily-app/server/app.js
+++ node-with-react-fullstack-web-dev/emaily-app/server/app.js
@@ -1,8 +1,11 @@
 const bodyParser = require('body-parser')
+const chalk = require('chalk')
 const helmet = require('helmet')
 const cors = require('cors')
+const compression = require('compression')
 const responseTime = require('response-time')
 const cookieSession = require('cookie-session')
+const expressValidator = require('express-validator')
 const passport = require('passport')
 const config = require('configuration')
 const database = require('database')
@@ -17,6 +20,7 @@ require('api/users/model')
 require('auth/config')
 
 app.use(responseTime())
+app.use(compression())
 // security
 app.use(cors())
 app.use(helmet())
@@ -26,6 +30,10 @@ if(env === 'dev') {
 }
 // content-type allowed
 app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+// express validator and sanitizer
+app.use(expressValidator())
 // cookies
 app.use(
   cookieSession({
@@ -37,26 +45,32 @@ app.use(
 app.use(passport.initialize())
 app.use(passport.session())
 
+app.disable('x-powered-by')
+
+// routes
 app.use('/', router)
 
 // handle the next(error) calls
-app.use((info, req, res, next) => {
-  const { status, message } = info
-  if(env === 'dev') {
-    console.log(JSON.stringify(info, null, 2))
-  }
-  res.status(status).send(message)
-})
+if(env === 'dev') {
+  app.use(require('errorhandler')())
+} else {
+  app.use((info, req, res, next) => {
+    const { status, message } = info
+    console.error(JSON.stringify(info, null, 2))
+    res.status(status).send(message)
+  })
+}
 
 exports.app = app
 exports.start = async () => {
   try {
     await database.connect()
-    console.log('Connected to database')
+    console.log('%s Connected to database', chalk.green('✓'))
     await app.listen(port)
-    console.log(`Connected on port: ${port}`)
+    console.log('%s App running at %d in %s mode', chalk.green('✓'), port, env)
+    console.log('  Press CTRL-C to stop\n')
   } catch (error) {
-    console.log('Something went wrong')
-    console.log(error)
+    console.error('Something went wrong')
+    console.error(error)
   }
 }
