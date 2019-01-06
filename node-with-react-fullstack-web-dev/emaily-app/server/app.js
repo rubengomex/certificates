ccb900ea49e3a41f2be39ac6a03f4232aa41cbac
Rúben Gomes <gomesruben21@gmail.com>
2019-01-06 14:53:35 +0000

    Prepare deployment for client build with heroku-postbuild command

--- node-with-react-fullstack-web-dev/emaily-app/server/app.js
+++ node-with-react-fullstack-web-dev/emaily-app/server/app.js
@@ -1,3 +1,4 @@
+const express = require('express')
 const bodyParser = require('body-parser')
 const chalk = require('chalk')
 const helmet = require('helmet')
@@ -9,9 +10,9 @@ const expressValidator = require('express-validator')
 const passport = require('passport')
 const config = require('configuration')
 const database = require('database')
-const app = require('express')()
 const port = config.get('PORT')
 const env = config.get('NODE_ENV')
+const app = express()
 
 // Ensures that User collection is created when we boot the app
 require('api/users/model')
@@ -47,12 +48,26 @@ app.use(passport.session())
 // routes
 app.use('/', require('routing'))
 
-// handle the next(error) calls
+// handle the next(error) calls (development)
 if(env === 'dev') {
   app.use(require('errorhandler')())
 } else {
+  // production
+  // Express will serve up production assets/ static files
+  // like our main.js file, or main.css file
+  app.use(express.static('client/build'))
+
+  // Express will serve up the index.html file
+  // if it doesn't recognized the route
+  const path = require('path')
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  )
+
+  // handle error properly
+  const { HTTP_STATUS } = require('utils/http')
   app.use((info, req, res, next) => {
-    const { status, message } = info
+    const { status = HTTP_STATUS.SERVER_ERROR, message } = info
     console.error(JSON.stringify(info, null, 2))
     res.status(status).send(message)
   })
