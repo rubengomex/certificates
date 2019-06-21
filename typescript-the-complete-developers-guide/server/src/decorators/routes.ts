2bd18d53ace1283b3a1b49e3861824fc72064700
Rúben Gomes <gomesruben21@gmail.com>
2019-06-21 09:29:01 +0100

    Finish server with type checking user property description

--- typescript-the-complete-developers-guide/server/src/decorators/routes.ts
+++ typescript-the-complete-developers-guide/server/src/decorators/routes.ts
@@ -1,10 +1,16 @@
 import 'reflect-metadata'
 import { Methods } from './Methods'
 import { MetadataKeys } from './MetadataKeys'
+import { RequestHandler } from 'express'
+
+interface RouteHandlerDescriptor extends PropertyDescriptor {
+  value?: RequestHandler
+}
 
 export const routeBinder = (method: string) => (path: string) => (
   target: any,
-  key: string
+  key: string,
+  desc: RouteHandlerDescriptor
 ) => {
   Reflect.defineMetadata(MetadataKeys.PATH, path, target, key)
   Reflect.defineMetadata(MetadataKeys.METHOD, method, target, key)
